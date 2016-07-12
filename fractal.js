71c0ad369825919ea274d745f42467e07ee7d531
Mark Perkins <mark@allmarkedup.com>
2016-07-12 09:31:41 +0100

    Fix deployment

--- fractal.js
+++ fractal.js
@@ -46,7 +46,7 @@ fractal.docs.set('path', `${__dirname}/docs`);
  */
 
 fractal.web.set('server.sync', true);
-fractal.web.set('builder.dest', 'build');
+fractal.web.set('builder.dest', 'dist');
 
 fractal.web.theme(require('./theme')({
 
