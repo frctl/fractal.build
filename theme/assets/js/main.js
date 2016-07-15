10df5a7ef5792a37323f4e03abe6af9d47e66ebb
Mark Perkins <mark@allmarkedup.com>
2016-07-15 15:01:35 +0100

    Fix up JS

--- theme/assets/js/main.js
+++ theme/assets/js/main.js
@@ -3,9 +3,20 @@ if (window.location.protocol.indexOf('file') !== 0) {
     const Pjax = require('pjax');
     new Pjax({
         elements:'.Frame-body a',
-        selectors: ['title', '.Frame-main']
+        selectors: ['title', '.Frame-main', '.Tree']
     });
-    document.addEventListener('pjax:success', function(e){
-        document.querySelectorAll('.Tree a')
-    });
+}
+
+function toArray(nodeList) {
+    return [].slice.call(nodeList);
+}
+
+function $(s, context){
+    context = context || document;
+    return context.querySelector(s);
+}
+
+function $$(s, context){
+    context = context || document;
+    return toArray(context.querySelectorAll(s));
 }
